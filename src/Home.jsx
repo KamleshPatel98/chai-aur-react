5c150c9d41e9f40c0f940abfbd1238a16c2df114
KamleshPatel98 <kamleshp52170@gmail.com>
2025-10-04 00:25:56 +0530

    dynamic categories

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,3 +1,5 @@
+import React, { useState, useEffect } from 'react'
+
 const slider1 = "slider 1";
 const sliders = [
    {'id': 1, 'img': 'https://drabhimanyu.com/assets/web/images/logo10.webp'},
@@ -14,12 +16,31 @@ const sliders2 = [
 ];
 
 function Home() {
+    const [data, setData] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        fetch('https://sld.itmingo.com/api/categories')
+        .then(res => res.json())
+        .then(json => {
+            setData(json.data)
+            setLoading(false);
+        });
+    }, []);
+
     return (
         <>
-            {sliders2.map((slider) => (
+            {/* {sliders2.map((slider) => (
                 <div key={slider.id}>
                     <h2>Home Slider {slider.id}</h2>
                     <img src={slider.img} alt={`Slider ${slider.id}`} width='100' height='100' />
+                </div>
+            ))} */}
+
+            {data.map((category) => (
+                <div key={category.id}>
+                    <h2>Home category {category.name}</h2>
+                    <img src={category.icon} alt={`category ${category.name}`} width='100' height='100' />
                 </div>
             ))}
         </>
