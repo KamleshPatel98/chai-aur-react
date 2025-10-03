e0189da1911a7214044675a139e38ffb7bfedf0b
KamleshPatel98 <kamleshp52170@gmail.com>
2025-10-03 23:51:54 +0530

    loog

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,8 +1,27 @@
+const slider1 = "slider 1";
+const sliders = [
+   {'id': 1, 'img': 'https://drabhimanyu.com/assets/web/images/logo10.webp'},
+   {'id': 2, 'img': 'https://drabhimanyu.com/assets/web/images/logo10.webp'},
+   {'id': 3, 'img': 'https://drabhimanyu.com/assets/web/images/logo10.webp'},
+   {'id': 4, 'img': 'https://drabhimanyu.com/assets/web/images/logo10.webp'},
+];
+
+const sliders2 = [
+   {'id': 1, 'img': '/src/assets/img.webp'},
+   {'id': 2, 'img': '/src/assets/img.webp'},
+   {'id': 3, 'img': '/src/assets/img.webp'},
+   {'id': 4, 'img': '/src/assets/img.webp'},
+];
 
 function Home() {
     return (
         <>
-            <h2>Home Page</h2>
+            {sliders2.map((slider) => (
+                <div key={slider.id}>
+                    <h2>Home Slider {slider.id}</h2>
+                    <img src={slider.img} alt={`Slider ${slider.id}`} width='100' height='100' />
+                </div>
+            ))}
         </>
     )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -3,22 +3,22 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 
-const slider = React.createElement (
-  'a',
-  {href: "http://google.com", target: '_blank'},
-  'CLick Google Link',
-)
+// const slider = React.createElement (
+//   'a',
+//   {href: "http://google.com", target: '_blank'},
+//   'CLick Google Link',
+// )
 
-const gallary = React.createElement (
-  'img',
-  {src: "https://media.istockphoto.com/id/517188688/photo/mountain-landscape.jpg?s=612x612&w=0&k=20&c=A63koPKaCyIwQWOTFBRWXj_PwCrR4cEoOw2S9Q7yVl8=", width: '300'},
-)
+// const gallary = React.createElement (
+//   'img',
+//   {src: "https://media.istockphoto.com/id/517188688/photo/mountain-landscape.jpg?s=612x612&w=0&k=20&c=A63koPKaCyIwQWOTFBRWXj_PwCrR4cEoOw2S9Q7yVl8=", width: '300'},
+// )
 
 createRoot(document.getElementById('root')).render(
-  // <StrictMode>
-  //   <App />
-  // </StrictMode>,
+  <StrictMode>
+    <App />
+  </StrictMode>,
 
   // slider
-  gallary
+  // gallary
 )
